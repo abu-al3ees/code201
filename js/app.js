ff3279289a7dcc6cb8776856fc558e45e56bbc99
abu-al3ees <malkhazali83@gmail.com>
2021-02-09 19:41:32 +0200

    edit 2

--- js/app.js
+++ js/app.js
@@ -3,53 +3,70 @@
 alert('do you think you know me lets get started ^_^');
 let counter=0;
 let nAme=prompt('Enter yor name first');
-let q1=prompt('do i like the football ?');
-q1.toLowerCase();
+let football=prompt('do i like the football ?');
+football.toLowerCase();
 //console.log(q1);
-if(q1==='yes'||q1==='y'){
+while (football !== 'yes' && football !== 'y'&& football !== 'no'&& football !== 'n') {
+  football=prompt('plz enter right ');
+}
+if(football==='yes'||football==='y'){
   alert('good you got it');
   counter++;
 }
-else{
+else if(football==='no'||football==='n'){
   alert('ops you faild  lets try another qustion');
 }
-let q2=prompt('do i like programing ?');
-q2.toUpperCase();
+let programing=prompt('do i like programing ?');
+programing.toUpperCase();
 //console.log(q2);
-if(q2==='NO'||q2==='N'){
+while (programing.toUpperCase() !== 'YES' && programing.toUpperCase() !== 'Y'&& programing.toUpperCase() !== 'NO'&& programing.toUpperCase() !== 'N') {
+  programing=prompt('plz enter right ');
+}
+if(programing.toUpperCase() ==='NO'||programing.toUpperCase() ==='N'){
   alert('you right XD');
   counter++;
 }
-else{
+// eslint-disable-next-line no-dupe-else-if
+else if(programing.toUpperCase() === 'NO'|| programing.toUpperCase() === 'N'){
   alert('you did not expect that XD');
 }
-let q3=prompt('dose the maqluba is my favarute food ?');
-q3.toLowerCase();
+let food=prompt('dose the maqluba is my favarute food ?');
+food.toLowerCase();
 //console.log(q3);
-if(q3==='yes'||q3==='y'){
+while (food.toLowerCase() !== 'yes' && food.toLowerCase() !== 'y'&& food.toLowerCase() !== 'no'&& food.toLowerCase() !== 'n') {
+  food=prompt('plz enter right ');
+}
+if(food.toLowerCase()==='yes'||food.toLowerCase()==='y'){
   alert('you are right good');
   counter++;
-}else{
+}else if(food.toLowerCase()==='no'|| food.toLowerCase()==='n'){
   alert('you are faild');
 }
-let q4=prompt('do i like the action movies');
-q4.toUpperCase();
+let movie=prompt('do i like the action movies');
+movie.toUpperCase();
 //console.log(q4);
-if(q4==='YES'||q4==='Y'){
+while (movie.toUpperCase() !== 'YES' && movie.toUpperCase() !== 'Y'&& movie.toUpperCase() !== 'NO'&& movie.toUpperCase() !== 'N') {
+  movie=prompt('plz enter right ');
+}
+if(movie.toUpperCase()==='YES'||movie.toUpperCase() ==='Y'){
   alert('good you are right');
   counter++;
-}else{
+}else if(movie.toUpperCase() === 'NO' || movie.toUpperCase() === 'N'){
   alert('false ^_^');
 }
-let q5=prompt('do i like my basketball');
-q5.toLowerCase();
+let basketball=prompt('do i like my basketball');
+basketball.toLowerCase();
 //console.log(q5);
-if(q5==='no'||q5==='n'){
-  alert('sure i dont like it');
+while (basketball.toLowerCase() !== 'yes' && basketball.toLowerCase() !== 'y'&& basketball.toLowerCase() !== 'no'&& basketball.toLowerCase() !== 'n') {
+  basketball=prompt('plz enter right ');
+}
+if(basketball.toLowerCase()==='no'||basketball.toLowerCase()==='n'){
+  alert('yes i dont like it');
   counter++;
 
-}else{
-  alert('you are wrong');
+// eslint-disable-next-line no-dupe-else-if
+}else if(basketball.toLowerCase() === 'no'&& basketball.toLowerCase() === 'n'){
+  alert('no you are wrong');
 }
 alert(`Thank you ${nAme} and you MARK ${counter} of 5`);
 

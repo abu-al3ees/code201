fa7aa89f042d42330db640eeb405471133f0ae13
abu-al3ees <66879546+abu-al3ees@users.noreply.github.com>
2021-02-09 13:48:51 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ let nAme=prompt('Enter yor name first');
 let q1=prompt('do i like the football ?');
 q1.toLowerCase();
 //console.log(q1);
-if(q1==='yes'){
+if(q1==='yes'||q1==='y'){
   alert('good you got it');
   counter++;
 }
@@ -16,7 +16,7 @@ else{
 let q2=prompt('do i like programing ?');
 q2.toUpperCase();
 //console.log(q2);
-if(q2==='NO'){
+if(q2==='NO'||q2==='N'){
   alert('you right XD');
   counter++;
 }
@@ -26,7 +26,7 @@ else{
 let q3=prompt('dose the maqluba is my favarute food ?');
 q3.toLowerCase();
 //console.log(q3);
-if(q3==='yes'){
+if(q3==='yes'||q3==='y'){
   alert('you are right good');
   counter++;
 }else{
@@ -35,7 +35,7 @@ if(q3==='yes'){
 let q4=prompt('do i like the action movies');
 q4.toUpperCase();
 //console.log(q4);
-if(q4==='YES'){
+if(q4==='YES'||q4==='Y'){
   alert('good you are right');
   counter++;
 }else{
@@ -44,12 +44,12 @@ if(q4==='YES'){
 let q5=prompt('do i like my basketball');
 q5.toLowerCase();
 //console.log(q5);
-if(q5==='no'){
-  alert('yes i dont like it');
+if(q5==='no'||q5==='n'){
+  alert('sure i dont like it');
   counter++;
 
 }else{
-  alert('no you are wrong');
+  alert('you are wrong');
 }
 alert(`Thank you ${nAme} and you MARK ${counter} of 5`);
 

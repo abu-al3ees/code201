f6c933658731d533a151f23709e3877b76c31e1c
abu-al3ees <malkhazali83@gmail.com>
2021-02-09 22:02:23 +0200

    add tow question

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # code201
 about me qz
+add 2 more question in the game
--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ let football=prompt('do i like the football ?');
 football.toLowerCase();
 //console.log(q1);
 while (football !== 'yes' && football !== 'y'&& football !== 'no'&& football !== 'n') {
-  football=prompt('plz enter right ');
+  football=prompt('plz enter right answer ');
 }
 if(football==='yes'||football==='y'){
   alert('good you got it');
@@ -20,7 +20,7 @@ let programing=prompt('do i like programing ?');
 programing.toUpperCase();
 //console.log(q2);
 while (programing.toUpperCase() !== 'YES' && programing.toUpperCase() !== 'Y'&& programing.toUpperCase() !== 'NO'&& programing.toUpperCase() !== 'N') {
-  programing=prompt('plz enter right ');
+  programing=prompt('plz enter right answer');
 }
 if(programing.toUpperCase() ==='NO'||programing.toUpperCase() ==='N'){
   alert('you right XD');
@@ -34,7 +34,7 @@ let food=prompt('dose the maqluba is my favarute food ?');
 food.toLowerCase();
 //console.log(q3);
 while (food.toLowerCase() !== 'yes' && food.toLowerCase() !== 'y'&& food.toLowerCase() !== 'no'&& food.toLowerCase() !== 'n') {
-  food=prompt('plz enter right ');
+  food=prompt('plz enter right answer ');
 }
 if(food.toLowerCase()==='yes'||food.toLowerCase()==='y'){
   alert('you are right good');
@@ -46,7 +46,7 @@ let movie=prompt('do i like the action movies');
 movie.toUpperCase();
 //console.log(q4);
 while (movie.toUpperCase() !== 'YES' && movie.toUpperCase() !== 'Y'&& movie.toUpperCase() !== 'NO'&& movie.toUpperCase() !== 'N') {
-  movie=prompt('plz enter right ');
+  movie=prompt('plz enter right answer answer');
 }
 if(movie.toUpperCase()==='YES'||movie.toUpperCase() ==='Y'){
   alert('good you are right');
@@ -58,17 +58,63 @@ let basketball=prompt('do i like my basketball');
 basketball.toLowerCase();
 //console.log(q5);
 while (basketball.toLowerCase() !== 'yes' && basketball.toLowerCase() !== 'y'&& basketball.toLowerCase() !== 'no'&& basketball.toLowerCase() !== 'n') {
-  basketball=prompt('plz enter right ');
+  basketball=prompt('plz enter right answer');
 }
 if(basketball.toLowerCase()==='no'||basketball.toLowerCase()==='n'){
   alert('yes i dont like it');
   counter++;
 
 // eslint-disable-next-line no-dupe-else-if
-}else if(basketball.toLowerCase() === 'no'&& basketball.toLowerCase() === 'n'){
+}else if(basketball.toLowerCase() === 'yes'&& basketball.toLowerCase() === 'y'){
   alert('no you are wrong');
 }
-alert(`Thank you ${nAme} and you MARK ${counter} of 5`);
+
+//gess a number
+let gessmyage=prompt('can you gess my age');
+gessmyage=parseInt(gessmyage);
+// eslint-disable-next-line no-undef
+let flag=true;
+for (let index = 1; index <= 4; index++) {
+  if(parseInt(gessmyage)===23){
+    alert('you are write');
+    flag=false;
+    break;
+  }else if(gessmyage>23){
+    alert('too high');
+  }else if(gessmyage<32){
+    alert('too low');
+  }
+  gessmyage=prompt('not correct gess again plz');
+
+}
+
+if(flag){
+  alert('you faild the right answer is 23');
+}
+let favcolors=['red','black','white','green','blue','yellow'];
+let found=false;
+for(let i=1;i<=6;i++){
+  let mycolor=prompt('can you gess my color?');
+  for(let j=0; j < favcolors.length; j++){
+    if(mycolor===favcolors[j]){
+      found=true;
+      alert('you win');
+      counter++;
+      break;
+    }
+  }
+  if(found===true){
+    alert('the right answer  '+favcolors);
+    break;
+  }
+}
+alert(`Thank you ${nAme} and you MARK ${counter} of 7`);
+
+
+
+
+
+
 
 
 
